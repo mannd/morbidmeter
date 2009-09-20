04ba10dd06d25590e45febd499f653d91bb616ec
David Mann <mannd@epstudiossoftware.com>
2009-09-20 13:56:27 -0400

    added methods to class User

--- user.py
+++ user.py
@@ -5,8 +5,23 @@
 
 from datetime import *
 
+def datetime_in_secs(date_time):
+    days = date_time.days
+    secs = date_time.seconds
+    return (days * 24 * 60 * 60 + secs)
+
 class User:
     """The user object."""
     def __init__(self, name):
         self.name = name
-    dob = datetime.now()
+    birthday = datetime.now()
+    longevity = 80.0
+    def time_alive(self):
+        return datetime.now() - self.birthday
+    def deathday(self):
+        daysalive = int(self.longevity * 365)
+        return self.birthday + timedelta(daysalive)
+    def lifepercent(self):
+        seconds_alive = datetime_in_secs(datetime.now() - self.birthday)
+        return float(seconds_alive) / float(self.longevity * 365 * 24 * 60 * 60)
+
